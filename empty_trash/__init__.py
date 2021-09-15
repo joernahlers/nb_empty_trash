c1274b4745721edc280da7011ef520805fffa67f
joernahlers <joern@jahlers.de>
2021-09-15 16:40:09 +0200

    Update __init__.py

--- empty_trash/__init__.py
+++ empty_trash/__init__.py
@@ -34,12 +34,26 @@ def _jupyter_nbextension_paths():
 class DeleteTrash(IPythonHandler):
     def delete(self):
         config = self.settings["trash_display_config"]
-        if os.path.isdir(config.trash_dir+"files/"):
-            for f in os.listdir(config.trash_dir+"files/"):
-                shutil.rmtree(config.trash_dir+"files/"+f)
-        if os.path.isdir(config.trash_dir+"info/"):
-            for f in os.listdir(config.trash_dir+"info/"):
-                shutil.rmtree(config.trash_dir+"info/"+f)
+        folder = config.trash_dir+"files/"
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        folder = config.trash_dir+"info/"
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
         self.finish("Trash deleted")
 
 
